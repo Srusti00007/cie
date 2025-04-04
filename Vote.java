a58286065dcbb4f08533a9099c07fece4c2474da
mithun <mca3@kletech.ac.in>
2025-04-04 11:20:36 +0530

    saveit

---
+++ Vote.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+public class Vote { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Enter age:");
+        int age = scanner.nextInt(); 
+
+        if (age < 18) {
+            System.out.println("Ineligible to vote");
+        } else {
+            System.out.println("Eligible to vote"); 
+        }
+
+        scanner.close(); 
+    }
+}
